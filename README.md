a54de7a765c34cef8dbd02cfcc1a5bd8437d5257
Yaroslav Tchernikoff <jaroslav.tchernikoff@yandex.ru>
2024-04-24 16:17:20 +0300

    первая попытка

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+* https://github.com/Tchernik0ff/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
+const item = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCards(name, link, removeCard) {
+  const template = item.cloneNode(true);
+  const removeBtn = template.querySelector('.card__delete-button');
+  template.querySelector('.card__image').src = link;
+  template.querySelector('.card__image').alt = name;
+  template.querySelector('.card__title').textContent = name;
+  removeBtn.addEventListener('click', removeCard);
+  placesList.append(template);
+}
 
-// @todo: Функция создания карточки
+function removeCard(event) {
+  const listitem = event.target.closest('.places__item');
+  listitem.remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(place => {
+  createCards(place.name,place.link, removeCard);
+});
